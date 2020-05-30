7795fe646eea59c53f5c3657da6590f41715da51
shunta.tokunaga <tokushun109@gmail.com>
2020-05-30 17:58:57 +0900

    viewを作成

--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,7 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # モデルの設定
 class Question(models.Model):
@@ -8,6 +11,11 @@ class Question(models.Model):
   # 公開時間を時間形式で
   # 名前はdata published
   pub_date = models.DateTimeField('data published')
+  def __str__(self):
+        return self.question_text
+  
+  def was_published_recently(self):
+    return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
 
@@ -16,3 +24,6 @@ class Choice(models.Model):
   question = models.ForeignKey(Question, on_delete=models.CASCADE)
   choice_text = models.CharField(max_length=200)
   votes = models.IntegerField(default=0)
+  
+  def __str__(self):
+        return self.choice_text
